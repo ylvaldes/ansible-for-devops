134cd206c9523b073adc19ead2d37ec3b1cc7749
Yasmani Ledesma Valdes <33784594+ylvaldes@users.noreply.github.com>
2020-11-23 15:48:35 -0300

    Upddate (#1)
    * Don't use sudo with pip3 installs in test environments.
    
    * Attempt to fix pip3 installs of ansible and molecule.
    
    * Try manually installing wheel for molecule Travis CI test.
    
    * Update build environment to focal fossa.
    
    * Test if molecule[docker] works correctly.
    
    * Make sure apt caches are updated more frequently for deployments and elk tests.
    
    * Bump Elasticsearch role requirement version.
    
    * Fix broken test build due to molecule docker dependency missing.
    
    * Switch from travis-ci.org to travis-ci.com.
    
    Co-authored-by: Jeff Geerling <geerlingguy@mac.com>

--- tests/collection.sh
+++ tests/collection.sh
@@ -3,13 +3,8 @@
 # Collection playbook tests.
 set -e
 
-# Make sure pip3 is available.
-sudo apt-get update
-sudo apt-get install -y python3-pip python3-setuptools
-pip3 install --upgrade setuptools pip
-
 # Install dependencies.
-sudo pip3 install ansible
+pip3 install ansible
 
 cd collection
 
